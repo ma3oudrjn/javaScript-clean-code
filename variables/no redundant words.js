e817ef18886bdcae4bfc9963f31d7b07037dbaae
Masoud Ranjbaran <masoudrng2@gmail.com>
2024-06-29 07:28:16 +0330

    no redundant

---
+++ variables/no redundant words.js
@@ -0,0 +1,11 @@
+const userData = {} //wrong ❌
+const user = {} // ok✅
+
+
+const productList = {} //wrong ❌
+const product = {} // ok✅
+
+
+
+const optionsInfo ={} //wrong ❌
+const options ={} // ok✅
