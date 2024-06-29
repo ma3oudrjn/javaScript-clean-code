4cdb65ed4d8c4508ae49a9ae5640a06eb270c791
Masoud Ranjbaran <masoudrng2@gmail.com>
2024-06-29 07:20:25 +0330

    proper naming rules

---
+++ variables/Proper naming.js
@@ -0,0 +1,21 @@
+// meaning full:
+const person = {id:121,name:"john"} //wrong ❌
+const admin = {id:122,name:"masoud"} // ok✅
+
+
+//pronounceable
+const yyymmdd = new Date() //wrong ❌
+const currentDate = new Date() // ok✅
+
+
+//detailed
+const time = 12 //wrong ❌
+const timeSinceLastCheck = 12 // ok✅
+
+//booleans
+// add verb in first of words
+const loading = true //wrong ❌
+const verify = false //wrong ❌
+
+const hasLoading = true // ok✅
+const isVerify = false // ok✅
