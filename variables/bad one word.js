89a3c8ea1b89a265704097e3c39ec506d5eef2af
Masoud Ranjbaran <masoudrng2@gmail.com>
2024-06-29 07:33:45 +0330

    bad one word

---
+++ variables/bad one word.js
@@ -0,0 +1,16 @@
+//wrong ❌
+const numbers = [1,2,3,4,5,6,7,8,9,]
+
+numbers.forEach(x => {
+    console.log("the number is " + x)
+})
+
+
+
+
+// ok✅
+const numbers = [1,2,3,4,5,6,7,8,9,]
+
+numbers.forEach(number => {
+    console.log("the number is " + number)
+})
