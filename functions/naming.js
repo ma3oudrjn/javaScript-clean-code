6aa90dd0e00eb0fd005e7171661e21a8882c38f1
Masoud Ranjbaran <masoudrng2@gmail.com>
2024-07-01 21:56:16 +0330

    function rules

---
+++ functions/naming.js
@@ -0,0 +1,11 @@
+// use verbs
+const saveNewPassword = ()=>{}
+const updatePassword = ()=>{}
+
+// consistent
+const getPassword = ()=>{}
+const fetchPassword = ()=>{}
+
+//detail
+const sum = ()=>{} //wrong ❌
+const sumOfMembers = ()=>{}// ok✅
